96ec8686d0efacb94ff90438f42f6620f2d032f4
Israel <adigun.jolly@gmail.com>
2025-02-08 17:57:53 -0600

    setup testable url paths

---
+++ backend/backend/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # JWT Auth endpoints
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Include additional endpoints from the accounts app
+    path('api/', include('api.urls')),
+]
