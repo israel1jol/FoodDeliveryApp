d07b5bc297916fdb0b2afd62b960d40c2e152cce
kevs01 <kevrs0311@gmail.com>
2025-02-10 21:13:00 -0600

    Learned how to use basic github

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 # accounts/urls.py
+from django.contrib import admin
 from django.urls import path
 from .views import dashboard
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('dashboard/', dashboard, name='dashboard'),
 ]
