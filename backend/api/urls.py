3a8744db7cbc99b1c91ca9093c2764a5f00758bd
kevs01 <kevrs0311@gmail.com>
2025-02-19 15:33:50 -0600

    Added instructions to run the project

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,7 @@
 # accounts/urls.py
-from django.contrib import admin
 from django.urls import path
 from .views import dashboard
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('dashboard/', dashboard, name='dashboard'),
 ]
