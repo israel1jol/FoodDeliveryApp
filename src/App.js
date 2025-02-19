6e676794890f62b023dc139e8207b285287a9c3d
Israel <adigun.jolly@gmail.com>
2025-02-19 12:27:00 -0600

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login';
 import Dashboard from './Components/Dashboard';
+import "./App.css";
 
 function App() {
   return (
