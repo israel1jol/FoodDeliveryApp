8dcc0d2bd05cac4e05eeb1689326868f93545223
Israel <adigun.jolly@gmail.com>
2025-03-28 11:16:25 -0500

    updated commit

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom";
+
+if (typeof TextEncoder === "undefined") {
+    const { TextEncoder, TextDecoder } = require("util");
+    global.TextEncoder = TextEncoder;
+    global.TextDecoder = TextDecoder;
+  }
+  
